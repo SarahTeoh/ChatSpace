f68a813d99d9f5a905225bbbfc349c8bfa6ec2d5
Sarah Teoh <you@example.com>
2020-01-16 14:46:34 +0900

    Add css design and final confirmation

--- server.php
+++ server.php
@@ -1,32 +1,43 @@
 <?php
 
-// set up host and port
+// Set up host and port
 $host = "127.0.0.1";
-$port = 8085;
+$port = 8081;
 header('Access-Control-Allow-Origin: *');
 
-// don't timeout!
+// Prevent time out
 set_time_limit(0);
-// create socket
+
+// Create socket
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
-// bind socket to port
+
+// Bind socket to port
 $result = socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
 
-// listen to the socket until termination
+// Listen to the socket until termination
 while(1)
 {
+    // Set maximum backlog of socket. In this case, only a maximum of 3 processes can be in queue
     socket_listen($socket, 3) or die("Could not set up socket listener\n");
+
+    // Accept client connecting to socket
     $client = socket_accept($socket);
 
+    // Read 1024 bytes input from socket
     $input = socket_read($client, 1024);
 
+    // Catch incoming request into array
     $incoming = array();
     $incoming = explode("\r\n", $input);
 
+    // Get name of file requested and method
     $fetchArray = array();
     $fetchArray = explode(" ", $incoming[0]);
 
+    // Name of file requested
     $file = $fetchArray[1];
+
+    // Show homepage
     if($file == "/"){
 
         $file = "show_topic.html";
@@ -37,29 +48,37 @@ while(1)
         $file = $filearray[1];
     }
 
+// Print out request method and file
 echo $fetchArray[0] . " Request " . $file . "\n";
-echo $file . "\n";
 
+// Variable to store output(Header + Content)
 $output = "";
+
+// Handle Ajax call
 $isAjax = explode("/", $filearray[1]);
 if($isAjax[0]=="ajax"){
     $Header = "HTTP/1.1 200 OK \r\n" .
     "Date: Fri, 1 Jan 2020 23:59:59 GMT \r\n" .
     "Content-Type: application/json ; charset=UTF-8 \r\n\r\n";
-  if($isAjax[1]=="data.json"){
+  if($isAjax[1]=="data.json"){ // Get data from json
     $Content = file_get_contents($file);
     }
-    else{
+    else{ // If POSTed from form
       if($fetchArray[0]=='POST'){
-        var_dump($GLOBALS);
           $parameters = explode("&", $incoming[15]);
           $user = urldecode(explode("=", $parameters[0])[1]);
           $message = urldecode(explode("=", $parameters[1])[1]);
-        if(isset($user) && isset($message)){
-            $Content = include($isAjax[1]);
+        if(isset($user) && isset($message)){ // Checks whether both parameters are set
+            $Content = include($isAjax[0] . "/" . $isAjax[1]); // If parameters are set, send to returnJson.php to process and catch the return
           }
       }
     }
+  }else if (explode(".", $filearray[1])[1]=="css") { // Handle css request
+    $Header = "HTTP/1.1 200 OK \r\n" .
+    "Date: Fri, 1 Jan 2020 23:59:59 GMT \r\n" .
+    "Content-Type: text/css; charset=UTF-8 \r\n\r\n";
+
+    $Content = file_get_contents($file);
   }else{
       $Header = "HTTP/1.1 200 OK \r\n" .
       "Date: Fri, 1 Jan 2020 23:59:59 GMT \r\n" .
@@ -70,7 +89,11 @@ if($isAjax[0]=="ajax"){
 
 $output = $Header . $Content;
 
+
+// Write output to socket
 socket_write($client,$output,strlen($output));
+
+// Close socket after done
 socket_close($client);
 }
 

65eaf3e9a7fb48b7f660e2263f6350e09e8d2cda
Teoh Tze Chuin(サラ) <sarah.teoh752@gmail.com>
2020-09-11 00:50:24 +0800

    Update returnJson.php

--- ajax/returnJson.php
+++ ajax/returnJson.php
@@ -1,13 +1,13 @@
 <?php
 
 $data = array("user" => $user, "message" => $message);
-$string = file_get_contents('data.json');
+$string = file_get_contents('./data.json');
 
 $json = json_decode($string);
 array_push($json->freeTalk, $data);
 $returnData = json_encode($data);
 $jsonData = json_encode($json, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-file_put_contents("data.json", $jsonData);
+file_put_contents("./data.json", $jsonData);
 return $returnData;
 exit;
 ?>

4ac945d1ca494a3683c6110a6c9da12a2c0c197b
Teoh Tze Chuin(サラ) <sarah.teoh752@gmail.com>
2020-09-11 14:56:01 +0800

    Update returnJson.php

--- ajax/returnJson.php
+++ ajax/returnJson.php
@@ -1,6 +1,6 @@
 <?php
 
-$data = array("user" => $user, "message" => $message);
+$data = array("user" => $_POST['user'], "message" => $_POST['message']);
 $string = file_get_contents('./data.json');
 
 $json = json_decode($string);

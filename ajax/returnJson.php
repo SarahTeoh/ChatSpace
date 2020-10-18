c874fe65c07b3057f65b851f0a817ad789b63f4d
Sarah <sarah@Sarahs-MacBook-Pro.local>
2020-10-18 20:13:13 +0900

    Fix server error

--- ajax/returnJson.php
+++ ajax/returnJson.php
@@ -1,6 +1,6 @@
 <?php
 
-$data = array("user" => $user, "message" => $message);
+$data = array("user" => $_POST['user'] , "message" => $_POST['message']);
 $string = file_get_contents('./data.json');
 
 $json = json_decode($string);
@@ -8,6 +8,6 @@ array_push($json->freeTalk, $data);
 $returnData = json_encode($data);
 $jsonData = json_encode($json, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
 file_put_contents("./data.json", $jsonData);
-return $returnData;
+echo $returnData;
 exit;
 ?>
